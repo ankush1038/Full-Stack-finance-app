da5cff1401b5ec9ca0493f8c09d222e5bb2ef2a2
its-ankur <ankuratul2007@gmail.com>
2023-12-05 10:21:13 +0530

    saving income in atlas

--- Backend/controllers/income.js
+++ Backend/controllers/income.js
@@ -9,5 +9,17 @@ exports.addIncome=async (req,res)=>{
         description,
         date
     });
+    try{
+        if(!title || !category || !description || !date){
+            return res.status(400).json({message:'All fields are required'});
+        }
+        if(amount<=0 || !amount==='number'){
+            return res.status(400).json({message:'Amount should be positive'});
+        }
+        await income.save();
+        res.status(200).json({message:'Income Added'});
+    }catch(err){
+            
+    }
     console.log(income);
 };
