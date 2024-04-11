8f46bb8beada539c770cd90fd445322b54108ff5
its-ankur <ankuratul2007@gmail.com>
2024-04-11 14:30:11 +0530

    Trying to make it work

--- Backend/controllers/income.js
+++ Backend/controllers/income.js
@@ -1,25 +1,39 @@
-const IncomeSchema=require('../models/IncomeModule');
+const IncomeSchema = require("../models/IncomeModule");
 
-exports.addIncome=async (req,res)=>{
-    const {title,amount,category,description,date}=req.body;
-    const income=IncomeSchema({
-        title,
-        amount,
-        category,
-        description,
-        date
-    });
-    try{
-        if(!title || !category || !description || !date){
-            return res.status(400).json({message:'All fields are required'});
-        }
-        if(amount<=0 || !amount==='number'){
-            return res.status(400).json({message:'Amount should be positive'});
-        }
-        await income.save();
-        res.status(200).json({message:'Income Added'});
-    }catch(err){
-            
+exports.addIncome = async (req, res) => {
+  const { title, amount, category, description, date } = req.body;
+  const income = IncomeSchema({
+    title,
+    amount,
+    category,
+    description,
+    date,
+  });
+  try {
+    if (!title || !category || !description || !date) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-    console.log(income);
+    if (amount <= 0 || !amount === "number") {
+      return res.status(400).json({ message: "Amount should be positive" });
+    }
+    await income.save();
+    res.status(200).json({ message: "Income Added" });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+  console.log(income);
+};
+
+exports.getIncomes = async (req, res) => {
+  try {
+    const incomes = await IncomeSchema.find().sort({ createdAt: -1 });
+    res.status(200).json({ incomes });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+exports.deleteIncome = async (req, res) => {
+  const { id } = req.params;
+  console.log(req.params);
 };
--- Backend/routes/transactions.js
+++ Backend/routes/transactions.js
@@ -1,4 +1,7 @@
-const router=require('express').Router();
-const {addIncome}=require('../controllers/income');
-router.post('/add-income',addIncome)
-module.exports=router;
+const { addIncome, getIncomes,deleteIncome } = require("../controllers/income");
+const router = require("express").Router();
+router
+  .post("/add-income", addIncome)
+  .get("/get-incomes", getIncomes)
+  .delete("/delete-income/:id", deleteIncome);
+module.exports = router;
