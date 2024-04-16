d274ec9be71f3f0ecd729893b40d1a24030b9ced
its-ankur <ankuratul2007@gmail.com>
2024-04-16 16:07:00 +0530

    side navbar and icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import styled from "styled-components";
 import bg from "./img/bg.png";
 import { MainLayout } from "./styles/Layouts";
 import Orb from "./components/Orb/Orb";
+import Navigation from "./components/Navigation/Navigation";
+import React,{ useState } from "react";
 function App() {
+  const [active, setActive] = React.useState(1);
   return (
   <AppStyled bg={bg} className="App">
     <Orb/>
     <MainLayout>
-      
+      <Navigation active={active} setActive={setActive}/>
     </MainLayout>
   </AppStyled>
 );
