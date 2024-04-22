fc4ccd8a746df556f409aefe8d4dd6edfe5eca61
ankush1038 <ankush1038.be21@chitkarauniversity.edu.in>
2024-04-22 22:37:19 +0530

    Components Checked

--- frontend/src/components/Incomes/Incomes.js
+++ frontend/src/components/Incomes/Incomes.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import { InnerLayout } from "../../styles/Layouts";
 import { useGlobalContext } from "../../context/globalContext";
-import Form from "../Form/Form";
+import Form from "../Form/form";
 import IncomeItem from "../IncomeItem/IncomeItem";
 function Incomes() {
   const { addIncome, incomes, getIncomes } = useGlobalContext();
