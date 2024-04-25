90435c6073a44f8e99f1c4bcec9447628cf54c60
its-ankur <ankuratul2007@gmail.com>
2024-04-25 15:13:26 +0530

    some formatting in backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,12 @@
-const express=require('express');
-const cors=require('cors'); 
-const {db}=require('./db/db');
-const {readdirSync}=require('fs');
-const { route } = require('./routes/transactions');
-const app=express();
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { db } = require("./db/db");
+const { readdirSync } = require("fs");
+const { route } = require("./routes/transactions");
+const app = express();
+require("dotenv").config();
 
-
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
 
 //middleware
 
@@ -18,13 +17,15 @@ app.use(cors());
 // });
 
 //routes
-readdirSync('./routes').map((route)=>app.use('/api/v1',require('./routes/'+route)));
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
 
-const server=()=>{
-    db();
-    app.listen(PORT,()=>{
-        console.log('Listening to port: ',PORT);
-    });
+const server = () => {
+  db();
+  app.listen(PORT, () => {
+    console.log("Listening to port: ", PORT);
+  });
 };
 
-server()
+server();
--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const db=async()=>{
     try{
-        mongoose.set('strictQuery', false);
         await mongoose.connect(process.env.MONGO_URL)
         console.log('Connected to database');
     } catch(err){
